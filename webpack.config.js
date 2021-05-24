dc24f4240c1c88d4f3e61b50fc0ff36041c887a1
ccaldwell <c-caitlin.caldwell@charter.com>
2021-05-24 11:47:52 -0600

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,33 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+ 
 module.exports = (env) => {
-  const isProduction = env === 'production';
-  const CSSExtract = new ExtractTextPlugin('styles.css');
-
+  const isProduction = env === "production";
+  const CSSExtract = new MiniCssExtractPlugin({ filename: "styles.css" });
+ 
   return {
-    entry: './src/app.js',
+    entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, 'public'),
-      filename: 'bundle.js'
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
     },
     module: {
       rules: [{
-        loader: 'babel-loader',
+        loader: "babel-loader",
         test: /\.js$/,
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: CSSExtract.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       }]
     },
     plugins: [
       CSSExtract
     ],
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devtool: isProduction ? "source-map" : "eval-cheap-module-source-map",
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
     }
   };
-  
 };
